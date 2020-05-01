f34a9d76bed35da2fbf347c2725e0255f643bcc7
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-05-01 16:38:23 +0300

    frontend for login

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
@@ -42,6 +43,7 @@ import { RegisterComponent } from './register/register.component';
     MatListModule,
     FormsModule,
     ReactiveFormsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
