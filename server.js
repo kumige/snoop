0db67b5c7c730f2c26cfe7e2d7a20864b0884166
Mikko <mikko1.romo@gmail.com>
2020-05-07 10:36:30 +0300

    https

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const graphQlHttp = require("express-graphql");
-const { graphqlUploadExpress } = require('graphql-upload')
+const { graphqlUploadExpress } = require("graphql-upload");
 const passport = require("./utils/passport");
 const schema = require("./schema/schema");
 const db = require("./db/db");
 const server = express();
+const path = require("path");
 
 server.use(cors());
-server.use(express.json()); 
-server.use(express.urlencoded({ extended: true })); 
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 server.use(express.static("public"));
-server.use('/uploads', express.static('uploads'))
+server.use("/uploads", express.static("uploads"));
 server.use("/modules", express.static("node_modules"));
 
 server.use((req, res, next) => {
@@ -34,6 +35,14 @@ server.use(
   (req, res) => {
     graphQlHttp({ schema, graphiql: true, context: { req, res } })(req, res);
   }
+);
+
+// redirect missing routes to index.html
+server.use((req, res) =>{
+  if (process.env.NODE_ENV === "production") {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+  }
+}
 );
 
 db.on("connected", () => {
